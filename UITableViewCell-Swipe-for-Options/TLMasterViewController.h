287dd409d4df97af3ce52bf744d34124ebd4bfc3
Ash Furrow <ash@ashfurrow.com>
2013-07-29 10:39:11 -0400

    Initial Commit

---
+++ UITableViewCell-Swipe-for-Options/TLMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  TLMasterViewController.h
+//  UITableViewCell-Swipe-for-Options
+//
+//  Created by Ash Furrow on 2013-07-29.
+//  Copyright (c) 2013 Teehan+Lax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLMasterViewController : UITableViewController
+
+@end
---
+++ UITableViewCell-Swipe-for-OptionsTests/UITableViewCell_Swipe_for_OptionsTests.m
@@ -0,0 +1,36 @@
+//
+//  UITableViewCell_Swipe_for_OptionsTests.m
+//  UITableViewCell-Swipe-for-OptionsTests
+//
+//  Created by Ash Furrow on 2013-07-29.
+//  Copyright (c) 2013 Teehan+Lax. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITableViewCell_Swipe_for_OptionsTests : XCTestCase
+
+@end
+
+@implementation UITableViewCell_Swipe_for_OptionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
