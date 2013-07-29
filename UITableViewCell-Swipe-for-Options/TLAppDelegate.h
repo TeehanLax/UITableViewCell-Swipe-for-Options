957985bab07045dc4ab97ce68bfd5d7c7cc83849
Ash Furrow <ash@ashfurrow.com>
2013-07-29 10:39:11 -0400

    Initial Commit

---
+++ UITableViewCell-Swipe-for-Options/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  UITableViewCell-Swipe-for-Options
+//
+//  Created by Ash Furrow on 2013-07-29.
+//  Copyright (c) 2013 Teehan+Lax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewCell-Swipe-for-Options/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewCell-Swipe-for-Options
+//
+//  Created by Ash Furrow on 2013-07-29.
+//  Copyright (c) 2013 Teehan+Lax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLAppDelegate class]));
+    }
+}
