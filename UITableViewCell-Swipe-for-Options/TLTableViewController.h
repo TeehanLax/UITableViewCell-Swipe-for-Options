964df47d330c09f3166d73d1cad87824f03b8456
Ash Furrow <ash@ashfurrow.com>
2013-07-29 16:48:25 -0400

    Put a toolbar on the bottom. Ugh storyboards.

---
+++ UITableViewCell-Swipe-for-Options/TLTableViewController.h
@@ -0,0 +1,26 @@
+//
+//  TLMasterViewController.h
+//  UITableViewCell-Swipe-for-Options
+//
+//  Created by Ash Furrow on 2013-07-29.
+//  Copyright (c) 2013 Teehan+Lax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TLTableViewController;
+
+@protocol TLTableViewControllerDelegate <NSObject>
+
+-(void)tableViewController:(TLTableViewController *)viewController didChangeEditing:(BOOL)editing;
+
+@end
+
+@interface TLTableViewController : UITableViewController
+
+@property (nonatomic, weak) id<TLTableViewControllerDelegate> delegate;
+
+- (void)insertNewObject:(id)sender;
+- (void)deleteSelectedCells;
+
+@end
