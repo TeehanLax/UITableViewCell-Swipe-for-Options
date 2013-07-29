3dcac5897ddd72df758f841070f2c9ac3536d6b4
Ash Furrow <ash@ashfurrow.com>
2013-07-29 15:29:42 -0400

    Initial attempt.

---
+++ UITableViewCell-Swipe-for-Options/TLSwipeForOptionsCell.m
@@ -0,0 +1,125 @@
+//
+//  TLSwipeForOptionsCell.m
+//  UITableViewCell-Swipe-for-Options
+//
+//  Created by Ash Furrow on 2013-07-29.
+//  Copyright (c) 2013 Teehan+Lax. All rights reserved.
+//
+
+#import "TLSwipeForOptionsCell.h"
+
+NSString *const TLSwipeForOptionsCellEnclosingTableViewDidBeginScrollingNotification = @"TLSwipeForOptionsCellEnclosingTableViewDidScrollNotification";
+
+#define kCatchWidth 180
+
+@interface TLSwipeForOptionsCell () <UIScrollViewDelegate>
+
+@property (nonatomic, weak) UIScrollView *scrollView;
+
+@property (nonatomic, weak) UIView *scrollViewContentView;
+@property (nonatomic, weak) UIView *scrollViewButtonView;
+
+@property (nonatomic, weak) UILabel *scrollViewLabel;
+
+@end
+
+@implementation TLSwipeForOptionsCell
+
+-(void)awakeFromNib {
+    [super awakeFromNib];
+    
+    [self setup];
+}
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+-(void)setup {
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 86)];
+    scrollView.contentSize = CGSizeMake(320 + kCatchWidth, 86);
+    scrollView.delegate = self;
+    scrollView.showsHorizontalScrollIndicator = NO;
+    
+    [self.contentView addSubview:scrollView];
+    self.scrollView = scrollView;
+    
+    UIView *scrollViewButtonView = [[UIView alloc] initWithFrame:CGRectMake(320.0f - kCatchWidth, 0, kCatchWidth, 86.0f)];
+    self.scrollViewButtonView = scrollViewButtonView;
+    [self.scrollView addSubview:scrollViewButtonView];
+    
+    UIButton *moreButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    moreButton.backgroundColor = [UIColor colorWithRed:0.78f green:0.78f blue:0.8f alpha:1.0f];
+    moreButton.frame = CGRectMake(0, 0, kCatchWidth / 2.0f, 86.0f);
+    [moreButton setTitle:@"More" forState:UIControlStateNormal];
+    [moreButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.scrollViewButtonView addSubview:moreButton];
+    
+    UIButton *deleteButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    deleteButton.backgroundColor = [UIColor colorWithRed:1.0f green:0.231f blue:0.188f alpha:1.0f];
+    deleteButton.frame = CGRectMake(kCatchWidth / 2.0f, 0, kCatchWidth / 2.0f, 86.0f);
+    [deleteButton setTitle:@"Delete" forState:UIControlStateNormal];
+    [deleteButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.scrollViewButtonView addSubview:deleteButton];
+    
+    UIView *scrollViewContentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 86)];
+    scrollViewContentView.backgroundColor = [[UIColor greenColor] colorWithAlphaComponent:0.9f];
+    [self.scrollView addSubview:scrollViewContentView];
+    self.scrollViewContentView = scrollViewContentView;
+    
+    UILabel *scrollViewLabel = [[UILabel alloc] initWithFrame:self.scrollViewContentView.bounds];
+    self.scrollViewLabel = scrollViewLabel;
+    [self.scrollViewContentView addSubview:scrollViewLabel];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(enclosingTableViewDidScroll) name:TLSwipeForOptionsCellEnclosingTableViewDidBeginScrollingNotification  object:nil];
+}
+
+-(void)enclosingTableViewDidScroll {
+    [self.scrollView setContentOffset:CGPointZero animated:YES];
+}
+
+#pragma mark - Overridden Methods
+
+-(void)setEditing:(BOOL)editing animated:(BOOL)animated {
+    [super setEditing:editing animated:animated];
+    
+    self.scrollView.scrollEnabled = !self.editing;
+}
+
+-(UILabel *)textLabel {
+    return self.scrollViewLabel;
+}
+
+#pragma mark - UIScrollViewDelegate Methods
+
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
+    
+    if (scrollView.contentOffset.x > kCatchWidth) {
+        targetContentOffset->x = kCatchWidth;
+    }
+    else {
+        *targetContentOffset = CGPointZero;
+        
+        // Need to call this subsequently to remove flickering. Strange. 
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [scrollView setContentOffset:CGPointZero animated:YES];
+        });
+    }
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView.contentOffset.x < 0) {
+        scrollView.contentOffset = CGPointZero;
+    }
+    
+    self.scrollViewButtonView.frame = CGRectMake(scrollView.contentOffset.x + (320.0f - kCatchWidth), 0.0f, kCatchWidth, 86.0f);
+}
+
+@end
+
+#undef kCatchWidth
