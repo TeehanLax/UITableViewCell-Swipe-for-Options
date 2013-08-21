990475a102df194ab3740104e50ffd6d672c2e08
Nicolas Gomollon <nicolasgomollon@yahoo.com>
2013-08-21 11:37:18 -0700

    Eliminates the annoying "AssertMacros" logs in a recent beta of iOS 7.
    Fix provided by Push Chen:
    https://devforums.apple.com/thread/197966?start=54&tstart=0

--- UITableViewCell-Swipe-for-Options/main.m
+++ UITableViewCell-Swipe-for-Options/main.m
@@ -9,7 +9,23 @@
 #import <UIKit/UIKit.h>
 #import "TLAppDelegate.h"
 
+typedef int (*PYStdWriter)(void *, const char *, int);
+static PYStdWriter _oldStdWrite;
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size) {
+	if (strncmp(buffer, "AssertMacros:", 13) == 0) {
+		return 0;
+	}
+	return _oldStdWrite(inFD, buffer, size);
+}
+
+void __iOS7B5CleanConsoleOutput(void) {
+	_oldStdWrite = stderr->_write;
+	stderr->_write = __pyStderrWrite;
+}
+
 int main(int argc, char * argv[]) {
+	__iOS7B5CleanConsoleOutput();
 	@autoreleasepool {
 		return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLAppDelegate class]));
 	}
