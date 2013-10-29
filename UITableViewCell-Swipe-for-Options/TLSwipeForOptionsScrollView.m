40d13ad07751825be4456570a5da0ff6d41fd3dd
Ash Furrow <ash@ashfurrow.com>
2013-10-29 10:28:08 -0400

    Fixes touchesX disparity.

--- UITableViewCell-Swipe-for-Options/TLSwipeForOptionsScrollView.m
+++ UITableViewCell-Swipe-for-Options/TLSwipeForOptionsScrollView.m
@@ -10,15 +10,22 @@
 
 @implementation TLSwipeForOptionsScrollView
 
-- (void) touchesBegan: (NSSet *)touches withEvent: (UIEvent *)event
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    if (self.tableViewCell)
-        [self.tableViewCell touchesBegan: touches withEvent: event];
+    [self.tableViewCell touchesBegan: touches withEvent: event];
 }
-- (void) touchesEnded: (NSSet *)touches withEvent: (UIEvent *)event
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    if (self.tableViewCell)
-        [self.tableViewCell touchesEnded:touches withEvent: event];
+    [self.tableViewCell touchesEnded:touches withEvent: event];
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.tableViewCell touchesMoved:touches withEvent:event];
+}
+
+-(void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.tableViewCell touchesCancelled:touches withEvent:event];
 }
 
 @end
